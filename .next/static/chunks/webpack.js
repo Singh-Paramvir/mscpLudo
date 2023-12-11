a72cfa63c955560d9193e402c2d1fb8b996f5127
Mac1 <mac1@Mac1-ka-Mac-mini.local>
2023-12-11 16:21:40 +0530

    Your commit message

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dd7af9651ed7a1b4"; }
+/******/ 		__webpack_require__.h = function() { return "1dacad73e8169fc2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
